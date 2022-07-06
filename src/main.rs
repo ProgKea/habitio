ce63e4d774a2aa4f318a254347b572db66169789
ProgKea <hoschy01@googlemail.com>
2022-07-06 08:00:27 +0200

    added errors

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate clap;
 
 const FILE_PATH: &str = "/home/keanu/code/habitio/src/habits";
 
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{App, AppSettings, Arg, ArgMatches, Error, SubCommand};
+use std::process::exit;
 use std::{
     fs,
     io::{Read, Write},
@@ -30,12 +31,14 @@ fn habit_exists(name: &str) -> bool {
 fn add(name: &str, desc: &str) {
     for char in name.chars() {
         if char.is_numeric() {
-            todo!()
+            eprintln!("The name can't contain numbers");
+            exit(1);
         }
     }
     for char in desc.chars() {
         if char.is_numeric() {
-            todo!()
+            eprintln!("The description can't contain numbers");
+            exit(1);
         }
     }
 
@@ -61,8 +64,8 @@ fn remove(name: &str) {
     let name = name.trim().replace(" ", "_");
 
     if !habit_exists(&name) {
-        // TODO: tell the user that the habit doesnt exist
-        todo!();
+        eprintln!("The habit doesn't exist");
+        exit(1);
     }
 
     let content = include_str!("/home/keanu/code/habitio/src/habits");
@@ -79,6 +82,7 @@ fn remove(name: &str) {
             let habit_name = habit.split_whitespace().nth(1).unwrap().trim();
             if name != habit_name {
                 new_content.push_str(habit);
+                new_content.push('\n');
             }
         }
     }
@@ -91,7 +95,6 @@ fn list() {
     let mut content = String::new();
 
     file.read_to_string(&mut content).unwrap();
-    let content = content.replace("_", " ");
 
     for habit in content.split("\n") {
         if !habit.is_empty() {
@@ -104,8 +107,8 @@ fn streak(name: &str) {
     let name = name.trim().replace(" ", "_");
 
     if !habit_exists(&name) {
-        // TODO: tell the user that the habit doesnt exist
-        todo!();
+        eprintln!("The habit doesn't exist");
+        exit(1);
     }
 
     let content = include_str!("/home/keanu/code/habitio/src/habits");
@@ -160,48 +163,47 @@ fn match_subcommands(arg_matches: ArgMatches) {
             "streak" => streak(subcommand.1.value_of("name").unwrap()),
             _ => {}
         }
-    } else {
-        todo!();
     }
 }
 
 fn main() {
     let matches = App::new("Habitio")
-        .about("Track your habits from the commandline")
-        .subcommand(
-            SubCommand::with_name("add")
-                .arg(
-                    Arg::with_name("name")
-                        .help("The Name of the habit you want to track (all whitespace will be replaced with underscore)")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("description")
-                        .help("Description of the habit you want to track")
-                        .required(true),
-                )
-                .about("Add a new habit to track"),
-        )
-        .subcommand(
-            SubCommand::with_name("remove")
-                .arg(
-                    Arg::with_name("name")
-                        .help("The Name of the habit you want to remove")
-                        .required(true),
-                )
-                .about("Remove a habit"),
-        )
-        .subcommand(SubCommand::with_name("list").about("List all habits"))
-        .subcommand(
-            SubCommand::with_name("streak")
-                .about("Increase the streak count of a habit")
-                .arg(
-                    Arg::with_name("name")
-                        .help("Name of the habit where you want to increase the streak")
-                        .required(true),
-                ),
-        )
-        .get_matches();
+	.about("Track your habits from the commandline")
+	.subcommand(
+	    SubCommand::with_name("add")
+		.arg(
+		    Arg::with_name("name")
+			.help("The Name of the habit you want to track (all whitespace will be replaced with underscore)")
+			.required(true),
+		)
+		.arg(
+		    Arg::with_name("description")
+			.help("Description of the habit you want to track")
+			.required(true),
+		)
+		.about("Add a new habit to track"),
+	)
+	.subcommand(
+	    SubCommand::with_name("remove")
+		.arg(
+		    Arg::with_name("name")
+			.help("The Name of the habit you want to remove")
+			.required(true),
+		)
+		.about("Remove a habit"),
+	)
+	.subcommand(SubCommand::with_name("list").about("List all habits"))
+	.subcommand(
+	    SubCommand::with_name("streak")
+		.about("Increase the streak count of a habit")
+		.arg(
+		    Arg::with_name("name")
+			.help("Name of the habit where you want to increase the streak")
+			.required(true),
+		),
+	)
+	.setting(AppSettings::ArgRequiredElseHelp)
+	.get_matches();
 
     match_subcommands(matches);
 }
